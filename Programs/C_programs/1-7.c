277e53718796b1991fe2165c6d23b215f9252eb4
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2024-12-05 20:54:49 -0500

    Got 1-7 to almost work.

--- Programs/C_programs/1-7.c
+++ Programs/C_programs/1-7.c
@@ -1,42 +1,56 @@
 #include <stdio.h>
 #define EOF -1
-/*Exercise 1-7 in the book: page 18*/
-/*Function to delete an element at index
-from an array.*/
-delElement(arr, index, size) 
-int** arr;
-int index;
-{
-    int len,j;
-    j = 0;
-    /*First, let's get the length of the 
-    inputted array:*/
-    for (len = 0; arr[len] != '\0'; len++);
-    printf("\n%d\n", len);
-}
+/*Exercise 1-7, page 18 in the book*/
 
-main() {    /*Outputs the user's input back
-            at them without consecutive blanks.*/
-    int c,j,e;
-    int usrIn[100]; /*Limit of 100 char input!*/
+main()
+{ /*Outputs the user's input back
+  at them without consecutive blanks.*/
+    int c, j, e;
+    char usrIn[100]; /*Limit of 100 char input!*/
 
-    for (j = 0; j < 100; j++) usrIn[j] = 0;
-    j = 0;
+    for (j = 0; j < 100; j++)
+        usrIn[j] = 0;
 
     /*First, get something from the
     user:*/
-    while (1) {
+    j = 0;
+    while (1)
+    {
         c = getchar();
-        if (c == EOF) break;
-        if (c != 8 && j < 100) {
+        if (c == EOF)
+            break;
+        if (c != 8 && j < 100)
+        {
             usrIn[j] = c;
             j++;
         }
     }
 
     printf("%s", "\n\n");
-    delElement(usrIn, 0);
-    int** newArr;
-    
-    for (j = 0; j < 100; j++) printf("%c", usrIn[j]);
+
+    /*Then, copy over the input without extra
+    spaces:*/
+    char newUsrIn[100];
+    e = 0; /*Index counter for new array*/
+    /*j is the index counter for old array*/
+    for (j = 0; usrIn[j] != '\0'; j++)
+    {
+        newUsrIn[e] = usrIn[j];
+        e++;
+        if (usrIn[j] == ' ')
+        {
+            /*When you find a space, move forward
+            past all consecutive spaces*/
+            while (usrIn[j] == ' ')
+            {
+                j++;
+            }
+            newUsrIn[e] = usrIn[j];
+            e++;
+        }
+    }
+
+    printf("%s", "\n");
+    for (j = 0; j < 100; j++)
+        printf("%c", newUsrIn[j]);
 }
