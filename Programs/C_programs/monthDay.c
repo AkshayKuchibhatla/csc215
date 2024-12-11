e07ec569a32d99235c2966de4823484d7eadbbdb
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2024-12-11 12:11:39 -0500

    Still working on monthday.c

--- Programs/C_programs/monthDay.c
+++ Programs/C_programs/monthDay.c
@@ -25,6 +25,35 @@ int year, dayNumber, *monthPtr, *dayPtr, *dtbl[]; {
     *dayPtr = dayNumber;
 }
 
+promptDayFromDate(m,d,y) 
+int* m,d,y; {
+    printf("Enter month number below.\n");
+    printf(">");
+    scanf("%d", m);
+    printf("\nEnter day number below.\n");
+    printf(">");
+    scanf("%d", d);
+    printf("\nEnter year number below.\n");
+    printf(">");
+    scanf("%d", y);
+}
+
+promptDateFromDay(n, y)
+int* n, y; {
+    printf("Enter day number of the year below.\n");
+    printf(">");
+    scanf("%d", y);
+    printf("\nEnter day number of the year below.\n");
+    printf(">");
+    scanf("%d", n);
+}
+
+getMonthName(n, mns) 
+int n;
+char *mns[]; {
+    return ((n < 1 || n > 12) ? mns[0] : mns[n]);
+}
+
 main() {
     int row1[13];
     row1[0] = 0;
@@ -60,16 +89,46 @@ main() {
     dayTable[0] = &row1;
     dayTable[1] = &row2;
 
-    int e, j;
+    char *monthNames[13];
+    monthNames[0] = "Illegal month";
+    monthNames[1] = "January";
+    monthNames[2] = "February";
+    monthNames[3] = "March";
+    monthNames[4] = "April";
+    monthNames[5] = "May";
+    monthNames[6] = "June";
+    monthNames[7] = "July";
+    monthNames[8] = "August";
+    monthNames[9] = "September";
+    monthNames[10] = "October";
+    monthNames[11] = "November";
+    monthNames[12] = "December";
+    
+    int option, month, day, year, dayNumber;
 
-    printf("%s", "\n");
+    while (1) {
+        printf("\n*** MAIN MENU ***\n");
+        printf("Choose one of the following options.\n\n");
+        printf("0: Get day number from date.\n");
+        printf("1: Get date from day number.\n\n");
 
-    printf("%d", numberDayOfYear(2024, 6, 18, dayTable));
-    printf("%s", "\n");
+        while (option != 48 && option != 49) {
+            printf(">");
+            option = getchar();
+            printf("\n");
+        }
 
-    int m,d;
-
-    dateOfNumberDay(2024, 84, &m, &d, dayTable);
-
-    printf("Day 84 is day %d of month %d.", d, m);
+        printf("\n");
+        if (option == 48) {
+            promptDayFromDate(&month, &day, &year);
+            printf(
+                "\nThe day number is %d.\n", 
+                numberDayOfYear(year, month, day, dayTable)
+            );
+        } else {
+            promptDateFromDay(&dayNumber, &year);
+            dateOfNumberDay(year, dayNumber, &month, &day, dayTable);
+            printf("\nThe date is %s %d, %d.\n", getMonthName);
+        }
+    }
 }
